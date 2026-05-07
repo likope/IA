e576755fe37f62b79afb3a663a81b3da780c92c2
User <you@example.com>
2026-05-07 14:39:29 +0200

    inizialize

---
+++ main.py
@@ -0,0 +1,12 @@
+#Import
+from langchain_core.runnables import RunnableLambda
+from langchain.ollama import ChatOllama
+
+
+#definisco un input generico di test
+test_input = "Hello, what is an LLM?"
+
+#definisco il modello di chat
+llm = 
+
+
