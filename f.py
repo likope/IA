e03bce7c058da27989fce9032c8ac03a2371032a
User <you@example.com>
2026-05-12 12:14:00 +0200

    1.0

--- f.py
+++
@@ -1,3 +0,0 @@
-#definisco una f che prende l input e output e costruisce la history relativa nel state
-def update_history(state: dict) -> str:
-    return state["history"] + f"User input: {state['input']}, Assistant output: {state['output']}"
